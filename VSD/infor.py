75fdc301c113605e257288f024ccdbe24af92fde
phong <xuanphongcht@gmail.com>
2022-06-29 14:15:41 +0700

    ngu

--- VSD/infor.py
+++ VSD/infor.py
@@ -18,16 +18,6 @@ def get_niemyet_infor(link):
     source_values = [i.text for i in soup.find_all('div', {'class':'col-md-8 col-sm-6 item-info item-info-main'})]
     return table, pd.DataFrame({'Keys':source_keys, 'Values':source_values})
 
-# for i in range(0, len(all_com.index)):
-#     symbol = all_com['Mã chứng khoán'].iloc[i]
-#     link = all_com['link_isin'].iloc[i]
-#     print(i, symbol, link)
-#     table1, table2 = get_niemyet_infor(link)
-#     # table1.to_csv(f'data_lake/niemyetbs/{symbol}.csv', index = False)
-#     # table2.to_csv(f'data_lake/infor/{symbol}.csv', index = False)
-#     table1.to_csv(f'/content/drive/MyDrive/Data Lake/Ingestion/Day 0/VSD/Volume/VolumeAdditionalEvents/{symbol}.csv', index = False)
-#     table2.to_csv(f'/content/drive/MyDrive/Data Lake/Ingestion/Day 0/VSD/Volume/VolumeNow/{symbol}.csv', index = False)
-
 all_com = pd.read_csv('isin.csv')
 def Crawl_Volume(i):
     symbol = all_com['Mã chứng khoán'].iloc[i]
